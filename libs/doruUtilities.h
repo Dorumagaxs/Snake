efd705245e05670d96b7a1f7b97103dfe5be8c2d
dorumagaxs <dorumagaxs.gmail.com>
2017-06-24 20:49:57 -0300

    borders added

---
+++ libs/doruUtilities.h
@@ -0,0 +1,3 @@
+void gotoxy(int x,int y) {
+    printf("%c[%d;%df",0x1B,y,x);
+}
--- src/Snake.c
+++ src/Snake.c
@@ -1,13 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "../libs/doruUtilities.h"
+
 int screenWidth, screenHeight;
 
-void printScreenBorder() {
-    char top[screenWidth+1];
-    char medium[screenWidth+1];
-    char bottom[screenWidth+1];
-    char none[screenWidth+1]; 
+void printScreenBorder(int width, int height) {
+    char line[width+1];
+    char noline[width+1];
+    int i;
+
+
+    line[0] = '|';
+    noline[0] = '|';
+    line[width-1] = '|';
+    noline[width-1] = '|';
+    line[width] = 0;
+    noline[width] = 0;
+
+    for (i=1; i<width-1; i++) {
+    	line[i] = '-';
+    	noline[i] = ' ';
+    }
+
+    gotoxy(1,1);
+    printf("%s", line);
+
+    for (i=2; i<5; i++) {
+    	gotoxy(1,i);
+    	printf("%s", noline);
+    }
+
+    gotoxy(1,5);
+    printf("%s", line);
+
+    for (i=6; i<height; i++) {
+    	gotoxy(1,i);
+    	printf("%s", noline);
+    }
+}
+
+void initGame() {
+	gotoxy(screenWidth/2, (screenHeight-5)/2); 
+}
+
+void menu() {
+	initGame();
 }
 
 int main(int argc, char *argv[]) {
@@ -17,10 +55,16 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
-    screenWidth = argv[1];
-    screenHeight = argv[2];
+    screenHeight = atoi(argv[1]);
+    screenWidth = atoi(argv[2]);
     
-    printScreenBorder();
+    system("clear");
+    printScreenBorder(screenWidth, screenHeight);
+
+    menu();
+
+    char b = getchar();
+    printf("%i", b);
 
 	return 0;
 }
